5dedc10eaeecc70b36c2a806a4302671232b882e
Andrew Wirt <awjwirt@gmail.com>
2018-07-02 03:48:29 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -31,5 +31,6 @@ class String
     count_sentences_array = split_self.select do |element|
       element.sentence? || element.question? || element.exclamation?
     end
+    count_sentences_array.length
   end
 end
